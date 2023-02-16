2f1f07f6a00f5f1f2c6e36616eac9c310b509de3
Pavel <pbezelt12@mail.ru>
2023-02-16 12:41:49 +0500

    First Commit

---
+++ main.rb
@@ -0,0 +1,30 @@
+puts "Введите число от 1 до 12"
+index=gets.to_i
+mouth=Array["Введите число от 1 до 12","Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"]
+case index
+
+when 1
+    puts "вы вывели месяц: #{mouth[index]}"
+when 2
+    puts "вы вывели месяц: #{mouth[index]}"
+when 3
+    puts "вы вывели месяц: #{mouth[index]}"
+when 4
+    puts "вы вывели месяц: #{mouth[index]}"
+when 5
+    puts "вы вывели месяц: #{mouth[index]}"
+when 6
+    puts "вы вывели месяц: #{mouth[index]}"
+when 8
+    puts "вы вывели месяц: #{mouth[index]}"
+when 9
+    puts "вы вывели месяц: #{mouth[index]}"
+when 10
+    puts "вы вывели месяц: #{mouth[index]}"
+when 11
+    puts "вы вывели месяц: #{mouth[index]}"
+when 12
+    puts "вы вывели месяц: #{mouth[index]}"
+else 
+    puts "Введите число от 1 до 12"
+end
